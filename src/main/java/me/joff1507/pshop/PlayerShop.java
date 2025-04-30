cfa3618bca1ed16cd1bbb7f090a9d0d6c248ca8e
jskandera <50663895+jskandera@users.noreply.github.com>
2025-04-30 19:01:26 +0200

    add holo and wg in main

--- src/main/java/me/joff1507/pshop/PlayerShop.java
+++ src/main/java/me/joff1507/pshop/PlayerShop.java
@@ -1,20 +1,29 @@
 package me.joff1507.pshop;
 
 import me.joff1507.pshop.command.CommandHandler;
-import me.joff1507.pshop.manager.ShopManager;
+import me.joff1507.pshop.manager.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerShop extends JavaPlugin {
     private static PlayerShop instance;
     private ShopManager shopManager;
+    private VaultManager vaultManager;
+    private HologramManager hologramManager;
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
         shopManager = new ShopManager(this);
+        vaultManager = new VaultManager(this);
+        hologramManager = new HologramManager(this);
         getCommand("pshop").setExecutor(new CommandHandler(this));
         getLogger().info("PlayerShop plugin enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PlayerShop plugin disabled.");
     }
 
     public static PlayerShop getInstance() {
@@ -23,5 +32,13 @@ public class PlayerShop extends JavaPlugin {
 
     public ShopManager getShopManager() {
         return shopManager;
+    }
+
+    public VaultManager getVaultManager() {
+        return vaultManager;
+    }
+
+    public HologramManager getHologramManager() {
+        return hologramManager;
     }
 }
